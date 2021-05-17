7bbf9d48f3f9d63f9922e3433074b3932b49973f
bhagvatula nipun <bhagvatula.nipun@tothenew.com>
2021-05-17 13:54:18 +0530

    adding changes to jenkins file

--- jenkins/vars/helper.groovy
+++
@@ -1,6 +0,0 @@
-def notifySlack(String color, message) {
-    slackSend channel: "${env.ICM_SLACK_CHANNEL}",
-            color: "${color}",
-            message: "${message}",
-            tokenCredentialId: "${env.ICM_SLACK_CRED_ID}"
-}
